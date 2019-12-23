88e47cd3ad4d1b52888ea84e0decf6b16abc3ac6
JDreamHeart <15602291936@163.com>
2019-12-23 22:03:01 +0800

    init project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("GmaeScript/Player")]
+
+public class Player : MonoBehaviour
+{
+    public float m_speed = 1;
+
+    public Transform m_rocket;
+
+    public float m_rocketTimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 纵向移动距离
+        float movev = 0;
+        // 水平移动距离
+        float moveh = 0;
+
+        m_rocketTimer -= Time.deltaTime;
+        if (m_rocketTimer <= 0) {
+            m_rocketTimer = 0.1f;
+            // 按空格键或鼠标左键发射子弹
+            if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) {
+                Instantiate(m_rocket, this.transform.position, this.transform.rotation);
+            }
+        }
+
+        // 按上键Z方向递增
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            movev += m_speed * Time.deltaTime;
+        }
+        // 按上键Z方向递减
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            movev -= m_speed * Time.deltaTime;
+        }
+        
+        // 按上键X方向递增
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            moveh += m_speed * Time.deltaTime;
+        }
+        // 按上键X方向递减
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            moveh -= m_speed * Time.deltaTime;
+        }
+        // 移动
+        this.transform.Translate(new Vector3(moveh, 0, movev));
+        // // 相当于
+        // this.transform.position += new Vector3(moveh, 0, movev);
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("GmaeScript/Rocket")]
+
+public class Rocket : MonoBehaviour
+{
+    public float m_speed = 10; // 子弹飞行速度
+
+    public float m_power = 1.0f; // 威力值
+
+    // 当可渲染的物体离开可视范围，此函数会被自动触发。
+    void OnBecameInvisible() {
+        if (this.enabled) { // 通过判断是否处于激活状态防止重复删除
+            Destroy(this.gameObject); // 当离开屏幕后销毁
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 向前（z方向）移动
+        transform.Translate(new Vector3(0,0,m_speed * Time.deltaTime));
+    }
+}
